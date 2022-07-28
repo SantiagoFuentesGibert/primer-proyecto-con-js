fbfacc207f100b6c87d36b14d3952f27f15a4a58
Santiago Fuentes <Santigibert6789@gmail.com>
2022-07-28 20:41:41 -0300

    Arreglando rutas y JS

--- js/index.js
+++ js/index.js
@@ -1,126 +1,98 @@
-//let opcion = parseInt(prompt("Eliga el numero del producto: 1-mother, 2-ram, 3-cooler y 4-procesador"));
-class Producto {
-    constructor(nombre,precio,codigo){
-        this.nombre = nombre
-        this.precio = precio
-        this.codigo = codigo
-    }
-    calcularIva(){
-        this.precio = this.precio * 1.21
-    }
-}
-const mother = new Producto("Mohther Z690 Aorus", 56000,1);
-const ram = new Producto("Ram Corsair", 25000,2);
-const cooler = new Producto("Cooler 240mm", 10000,3);
-const procesador = new Producto("Procesador i7", 80000,4);
-mother.calcularIva ();
-ram.calcularIva ();
-cooler.calcularIva ();
-procesador.calcularIva ();
-const productos = [];
-productos.push(mother)
-productos.push(ram)
-productos.push(cooler)
-productos.push(procesador)
-let precioFinal = document.getElementById("productos");
-let li = document.createElement("li");
-/* switch (opcion) {
-    case 1:
-        let btn1 = document.getElementById("1")
-        li.innerHTML = `<h2>Producto: ${mother.nombre}</h2>
-                        <b>$${mother.precio}</b>
-                        `;
-        precioFinal.append (li);
-        break;
-
-    case 2:
-        li.innerHTML = `<h2>Producto: ${ram.nombre}</h2>
-                        <b>$${ram.precio}</b>
-                        `;
-        precioFinal.append (li);
-        break;
-
-    case 3:
-        li.innerHTML = `<h2>Producto: ${cooler.nombre}</h2>
-                        <b>$${cooler.precio}</b>
-                        `;
-        precioFinal.append (li);
-        break;
-    
-    case 4:
-        li.innerHTML = `<h2>Producto: ${procesador.nombre}</h2>
-                        <b>$${procesador.precio}</b>
-                        `;
-        precioFinal.append (li);
-        break;
-    
-    default:
-        break;
-}  */
-
-//Mother
-const btnMother = () => {
-    li.innerHTML = `<h2>Producto: ${mother.nombre}</h2>
-                    <b>$${mother.precio}</b>
-                    `;
-    precioFinal.append (li);
-}
-
-let boton1 = document.getElementById(1);
-    boton1.addEventListener("click", btnMother);
-
-//Ram
-const btnRam = () => {
-    li.innerHTML = `<h2>Producto: ${ram.nombre}</h2>
-                    <b>$${ram.precio}</b>
-                    `;
-    precioFinal.append (li);
-}
-
-let boton2 = document.getElementById(2);
-    boton2.addEventListener("click", btnRam);
-
-//Cooler
-const btnCooler = () => {
-    li.innerHTML = `<h2>Producto: ${cooler.nombre}</h2>
-                    <b>$${cooler.precio}</b>
-                    `;
-    precioFinal.append (li);
-}
-
-    let boton3 = document.getElementById(3);
-    boton3.addEventListener("click", btnCooler);
-
-//Procesador
-const btnProcesador = () => {
-    li.innerHTML = `<h2>Producto: ${procesador.nombre}</h2>
-                    <b>$${procesador.precio}</b>
-                    `;
-    precioFinal.append (li);
-}
-
-    let boton4 = document.getElementById(4);
-    boton4.addEventListener("click", btnProcesador);
-
-
-
-
-
-
-
-
-// Carrito de compras
-
-/* const carrito = document.querySelector('#carrito');
+//Variables
+const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaProductos = document.querySelector('#lista-productos');
+let productosCarrito = [];
 
 registrarEventListeners();
 function registrarEventListeners() {
+    // Agregas un producto usando "Agregar al carrito"
     listaProductos.addEventListener('click', agregarProducto);
+    // Elimina productos del carrito
+    carrito.addEventListener('click', eliminarProducto);
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', () => {
+        productosCarrito = [];
+        limpiarHTML();
+    })
 }
 
+//Funciones
 function agregarProducto(e) {
-    console.log(e.target.classList)
-} */
+    if( e.target.classList.contains('agregar-carrito')){
+        const productoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosProducto(productoSeleccionado);
+    }
+
+    
+}
+
+function eliminarProducto(e) {
+    console.log(e.target.classList);
+    if(e.target.classList.contains('borrarProducto')) {
+        const productoId = e.target.getAttribute('data-id');
+        productosCarrito = productosCarrito.filter( producto => producto.id !== productoId);
+        carritoHTML();
+    }
+}
+
+// Lee el contenido del HTML que clickeamos y extrae la info
+function leerDatosProducto(producto) {
+    // Crear un objeto con el contenido del producto actual
+    const infoProducto = {
+        nombre: producto.querySelector('h3').textContent,
+        precio: producto.querySelector('span').textContent,
+        id: producto.querySelector('button').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    // Verifica si un elemento ya existe en el carrito
+    const existeProducto = productosCarrito.some( producto => producto.id === infoProducto.id);
+    console.log(existeProducto);
+    if(existeProducto) {
+        // Actualizamos la cantidad
+        const productoMas = productosCarrito.map( producto => {
+            if(producto.id === infoProducto.id){
+                producto.cantidad++;
+                return producto;
+            }else {
+                // Agregamos el elemento al arreglo del carrito
+                return producto;
+            }
+        } );
+        productosCarrito = [...productoMas];
+    }else {
+        productosCarrito = [...productosCarrito, infoProducto];
+    }
+    carritoHTML();
+}
+
+// Muestra el Carrito de compras en el HTML
+function carritoHTML() {
+    // Limpia el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    productosCarrito.forEach(producto => {
+        const {nombre, precio, cantidad, id} = producto;
+        const row = document.createElement('tr')
+        row.innerHTML = `
+                        <td>${nombre}</td>
+                        <td>${precio}</td>
+                        <td>${cantidad}</td>
+                        <td>
+                            <a href="#" class="borrarProducto" data-id="${id}"> X </a>
+                        </td>
+                        `;
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elimina los productos del tbody
+function limpiarHTML() {
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
